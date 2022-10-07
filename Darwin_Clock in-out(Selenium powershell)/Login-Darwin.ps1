1cf626f4ebe3d76e5a8ded86008b4d5eb283167c
Harsha0111 <111037498+Harsha0111@users.noreply.github.com>
2022-10-07 15:36:49 +0530

    Add files via upload

---
+++ Darwin_Clock in-out(Selenium powershell)/Login-Darwin.ps1
@@ -0,0 +1,18 @@
+﻿$scriptPath = "D:\Login-selenium"
+
+ADD-TYPE -Path "$scriptPath\webDriver.dll"
+
+$chrome = New-Object OpenQA.Selenium.Chrome.ChromeDriver
+
+$chrome.Manage().Window.Maximize()
+$chrome.Navigate().GoToUrl('https://nseit.darwinbox.in')
+
+$chrome.FindElement([OpenQA.Selenium.By]::Id("userNameInput")).SendKeys('user_name')
+$chrome.FindElement([OpenQA.Selenium.By]::Id("passwordInput")).SendKeys('user_password')
+$chrome.FindElement([OpenQA.Selenium.By]::Id("submitButton")).Click()
+Start-Sleep -Seconds 10
+
+$clock = $chrome.FindElement([OpenQA.Selenium.By]::xpath('//*[@id="attendance-logger-widget"]'))
+$chrome.ExecuteScript("arguments[0].click();",$clock)
+Start-Sleep -Seconds 5
+$chrome.Quit()
