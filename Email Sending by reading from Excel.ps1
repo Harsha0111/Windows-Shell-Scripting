5126f44bd6eb7aee026c0dc384a54e991ce0d2b5
Harsha JS <111037498+Harsha0111@users.noreply.github.com>
2022-10-14 00:00:13 +0530

    Add files via upload

---
+++ Email Sending by reading from Excel.ps1
@@ -0,0 +1,18 @@
+﻿$objExcel = New-Object -ComObject Excel.Application  
+$WorkBook = $objExcel.Workbooks.Open("D:\Scripting\Mail Scripting SpreadSheet.xlsx") 
+$workSheet = $WorkBook.Sheets.Item(1)
+
+$userName = "harshajsharsh@gmail.com"
+$password = "16 digit password"
+
+$from = "harshajsharsh@gmail.com"
+$to = $workSheet.cells.Item(2,1).Text
+
+
+$subject = $workSheet.cells.Item(2,2).Text
+$body = $workSheet.cells.Item(2,3).Text
+
+[SecureString]$securepassword = $password | ConvertTo-SecureString -AsPlainText -Force 
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $userName, $securepassword
+Send-MailMessage -SmtpServer smtp.gmail.com -Port 587 -UseSsl -From $from -To $to -Subject $subject -Body $body -Credential $credential
+write "Email sent successfully"
